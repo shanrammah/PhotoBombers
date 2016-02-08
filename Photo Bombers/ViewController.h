9d29c7946c541ab0c88312d5280071644691dec6
Shan Rammah <shan.rammah@gmail.com>
2016-02-07 20:22:17 -0800

    Initial Commit

---
+++ Photo Bombers/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Photo Bombers
+//
+//  Created by Shan Rammah on 2/7/16.
+//  Copyright © 2016 Shan Rammah. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Photo BombersTests/Photo_BombersTests.m
@@ -0,0 +1,39 @@
+//
+//  Photo_BombersTests.m
+//  Photo BombersTests
+//
+//  Created by Shan Rammah on 2/7/16.
+//  Copyright © 2016 Shan Rammah. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Photo_BombersTests : XCTestCase
+
+@end
+
+@implementation Photo_BombersTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
